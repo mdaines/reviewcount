3ea6f83acf7ccda9e1502f32857411ccb8189ccd
Michael Daines <md@slowscan.net>
2025-02-03 11:20:37 -0500

    Fix error description test

--- ReviewCountTests/WaniKaniErrorTests.swift
+++ ReviewCountTests/WaniKaniErrorTests.swift
@@ -3,7 +3,7 @@ import Testing
 
 struct WaniKaniErrorTests {
     @Test func localizedDescription() async throws {
-        #expect((WaniKaniError.parseError as Error).localizedDescription == "Couldn't parse the response from the server.")
+        #expect((WaniKaniError.parseError as Error).localizedDescription == "Couldn’t parse the response from the server.")
         #expect((WaniKaniError.unauthorizedError as Error).localizedDescription == "Invalid API token.")
         #expect((WaniKaniError.responseError(code: 404, message: "Not Found") as Error).localizedDescription == "The server returned an error response.")
         #expect((WaniKaniError.unexpectedResponseClassError as Error).localizedDescription == "The server returned an error response.")
